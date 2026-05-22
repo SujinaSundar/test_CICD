515b566d269f532ab418df763e0e8a51c66f0b2f
SujinaSundar <sujinasundar2025@gmail.com>
2026-05-22 14:59:10 +0530

    pylint issue solved

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,20 @@
+"""testing calc function"""
+import pytest
 from app import add
 
+
 def test_add_positive():
+    """test for positive"""
     assert add(2, 3) == 5
 
 def test_add_negative():
+    """test for negative"""
     assert add(-1, -1) == -2
 
 def test_add_zero():
+    """test for zero"""
     assert add(0, 0) == 0
 
-import pytest
 
 @pytest.mark.parametrize(
     "a,b,result",
@@ -20,4 +25,5 @@ import pytest
     ]
 )
 def test_add(a, b, result):
+    """test with parameters"""
     assert a + b == result
