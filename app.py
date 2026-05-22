87860122e3ad5c9b32bc61f1c6337868de75af3b
SujinaSundar <sujinasundar2025@gmail.com>
2026-05-22 15:50:03 +0530

    Fixed linting issues

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 """simple calc module"""
+
+
 def add(a, b):
     """Return the sum of two numbers."""
     return a + b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,25 @@
 """testing calc function"""
+
 import pytest
 from app import add
+
+
 def test_add_positive():
     """test for positive"""
     assert add(2, 3) == 5
+
 
 def test_add_negative():
     """test for negative"""
     assert add(-1, -1) == -2
 
+
 def test_add_zero():
     """test for zero"""
     assert add(0, 0) == 0
 
-@pytest.mark.parametrize(
-    "a,b,result",
-    [
-        (2, 3, 5),
-        (1, 1, 2),
-        (5, 5, 10)
-    ]
-)
+
+@pytest.mark.parametrize("a,b,result", [(2, 3, 5), (1, 1, 2), (5, 5, 10)])
 def test_add(a, b, result):
     """test with parameters"""
     assert a + b == result
